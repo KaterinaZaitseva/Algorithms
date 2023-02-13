23d11659c408597681c840d9de5a6a072feafce6
KaterinaZaitseva <katerinafolly2@gmail.com>
2023-02-14 01:37:16 +0300

    Correcrion

--- BinarySearch/SearchingGame.cs
+++ BinarySearch/SearchingGame.cs
@@ -11,30 +11,33 @@ namespace BinarySearch{
         public SearchingGame(int highBound) {
             _range = new int[highBound + 1];
 
-            for (int i = 0; i <= highBound; i++)
+            for (int i = 1; i <= highBound; i++)
                 _range[i] = i;
-        } 
+        }
 
-        public void GetStart() {
+        public void GetStart()
+        {
             int input;
             int high = _range.Length - 1;
-            int low = 0;    
-            do {
-                int mid = (low + high) / 2;
-                Console.WriteLine("The hidden number " + _range[mid]);
-
+            int low = 0;
+            int guess;
+            int mid = 0;
+            while (low <= high) {
+                mid = (low + high) / 2;
+                guess = _range[mid];
                 do {
+                    Console.WriteLine("The supposed number is " + guess);
                     Console.WriteLine("1. This number is less then the hidden number");
-                    Console.WriteLine("2. This number is bigger then the hidden number");
-                    Console.WriteLine("3. This number is the hidden number");
+                    Console.WriteLine("2. This number is bigger or equal to then the hidden number");
                     input = Convert.ToInt32(Console.ReadLine());
-                } while (input != 1 && input != 2 && input != 3);
+                } while (input != 1 && input != 2);
+                if (input == 1)
+                    high = mid - 1;
 
-                if(input == 1) high = mid;
-                else if (input == 2) low = mid + 1;
-                else Console.WriteLine("Urrrrrraaaaaaaaaaaaa");
-
-            } while (input != 3);
+                else if (input == 2)
+                    low = mid;
+            }
+            Console.WriteLine("The hidden number " + _range[mid]);
         }
     }
 }

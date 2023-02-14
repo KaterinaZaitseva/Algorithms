2a54325976236cc502b51177a402216359175313
KaterinaZaitseva <katerinafolly2@gmail.com>
2023-02-14 22:14:57 +0300

    Correction "BinarySearch"

--- BinarySearch/SearchingGame.cs
+++ BinarySearch/SearchingGame.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 namespace BinarySearch{
     internal static class SearchingGame {
 
-        public static void GetStart(int highBound)
-        {
+        public static void GetStart(int highBound) {
             int input;
             int high = highBound;
             int low = 0;
             int guess = 0;
             int mid = 0;
+
             while(low < high) {
                 mid = (low + high) / 2;
                 guess = mid;
@@ -23,13 +23,16 @@ namespace BinarySearch{
                     Console.WriteLine("2. This number is bigger or equal to then the hidden number");
                     input = Convert.ToInt32(Console.ReadLine());
                 } while (input != 1 && input != 2);
+
                 if (input == 1)
                     high = mid - 1;
 
                 else if (input == 2)
                     low = mid + 1;
             }
+
             Console.WriteLine("The hidden number " + high);
         }
+
     }
 }

d3eda4017a178c6a82c4c0082beda1e12f824fd2
KaterinaZaitseva <katerinafolly2@gmail.com>
2023-02-15 03:38:20 +0300

    Add the second FindPrimeNumbers n fix a return value

--- SieveOfEratosthenes/Program.cs
+++ SieveOfEratosthenes/Program.cs
@@ -1,12 +1,18 @@
-﻿namespace SieveOfEratosthenes
-{
-    internal class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.Write("Enter the range: ");
-            int input = Convert.ToInt32(Console.ReadLine());
-            SieveOfEratosthenes.FindPrimeNumbers(input);
-        }
+﻿namespace SieveOfEratosthenes;
+internal class Program { 
+    public static void Main(string[] args) {
+        List<int> result = new List<int>();
+        result = SieveOfEratosthenes.FindPrimeNumbers(101); 
+
+        foreach (int prime in result)
+            Console.Write(prime + " "); // 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101
+
+        Console.WriteLine();
+
+        result = SieveOfEratosthenes.FindPrimeNumbers(5, 13);
+
+        foreach (int prime in result)
+            Console.Write(prime + " "); // 5 7 11 13
     }
 }
+
--- SieveOfEratosthenes/SieveOfEratosthenes.cs
+++ SieveOfEratosthenes/SieveOfEratosthenes.cs
@@ -4,22 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SieveOfEratosthenes
+namespace SieveOfEratosthenes;
+internal static class SieveOfEratosthenes
 {
-    internal static class SieveOfEratosthenes
-    {
-        public static void FindPrimeNumbers(int n) {
-            bool[] isPrime = new bool[n];
-            Array.Fill(isPrime, true);
+    public static List<int> FindPrimeNumbers(int n) {
+        bool[] isPrime = new bool[n + 1];
+        List<int> primeNumbers = new List<int>();
+        Array.Fill(isPrime, true);
 
-            for (int i = 2; i * i < n; i++)
-                if (isPrime[i])
-                    for (int j = i * i; j < n; j += i)
-                        isPrime[j] = false;
+        for (int i = 2; i * i <= n; i++)
+            if (isPrime[i])
+                for (int j = i * i; j <= n; j += i)
+                    isPrime[j] = false;
 
-            for (int i = 2; i < n; i++)
-                if (isPrime[i])
-                    Console.Write(i + " ");
-        }
+        for (int i = 2; i <= n; i++)
+            if (isPrime[i])
+                primeNumbers.Add(i);
+        return primeNumbers;
     }
+
+    // предположительно ты хотел, чтобы булевый массив начинался с нижней границы,
+    // но я пока не придумала как это реализовать)
+    public static List<int> FindPrimeNumbers(int leftBound, int rightBound) {
+        List<int> primeNumbers = new List<int>();
+        bool[] isPrime = new bool[rightBound + 1];
+        Array.Fill(isPrime, true);
+
+        for (int i = 2; i * i <= rightBound; i++)
+            if (isPrime[i])
+                for (int j = i * i; j <= rightBound; j += i)
+                    isPrime[j] = false;
+
+        for (int i = leftBound; i <= rightBound; i++)
+            if (isPrime[i])
+                primeNumbers.Add(i);
+        return primeNumbers;
+    }
+
 }
+
